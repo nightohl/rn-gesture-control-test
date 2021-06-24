4622a1353e3363d3daa68e18246aa09f4952f7a0
nightohl <48432932+nightohl@users.noreply.github.com>
2021-06-24 20:48:08 +0900

    refac: default 함수를 프롭 안에 정의 (#2)

--- RNGestureControl/components/GestureContainer.tsx
+++ RNGestureControl/components/GestureContainer.tsx
@@ -1,32 +1,17 @@
 import React from 'react';
 import GestureRecognizer from 'react-native-swipe-gestures';
-import {GestureContainerProps, GestureHandler} from './types';
-
-const onSwipeUpDefault: GestureHandler = gestureState => {
-  console.log('state : ', gestureState);
-};
-
-const onSwipeDownDefault: GestureHandler = gestureState => {
-  console.log('state : ', gestureState);
-};
-
-const onSwipeLeftDefault: GestureHandler = gestureState => {
-  console.log('state : ', gestureState);
-};
-
-const onSwipeRightDefault: GestureHandler = gestureState => {
-  console.log('state : ', gestureState);
-};
+import {GestureContainerProps} from './types';
 
 export default function GestureContainer({
   children,
-  onSwipeUp = onSwipeUpDefault,
-  onSwipeDown = onSwipeDownDefault,
-  onSwipeLeft = onSwipeLeftDefault,
-  onSwipeRight = onSwipeRightDefault,
+  onSwipeUp = gestureState => console.log('state : ', gestureState),
+  onSwipeDown = gestureState => console.log('state : ', gestureState),
+  onSwipeLeft = gestureState => console.log('state : ', gestureState),
+  onSwipeRight = gestureState => console.log('state : ', gestureState),
   velocityThreshold = 0.3,
   directionalOffsetThreshold = 80,
 }: GestureContainerProps) {
+  //
   const config = {
     velocityThreshold,
     directionalOffsetThreshold,
